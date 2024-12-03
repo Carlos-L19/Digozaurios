d48bfd2caa10c090509fb8d357b09d407a28b8e5
GreysyBurgos <SesmaWTF@gmail.com>
2024-12-03 14:53:26 +0100

    Modificado el enemigo y el GameMangaer

---
+++ Assets/Scripts/EnemigoCalavera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoCalavera : MonoBehaviour
+{
+    [SerializeField] float velocidad;
+    [SerializeField] Camera camara;
+    [SerializeField] Vector2 posicionMinima;
+    [SerializeField] Vector2 posicionInicial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camara = Camera.main;
+        posicionMinima = camara.ViewportToWorldPoint(new Vector2(0, 0));
+        posicionInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+        if (transform.position.x < posicionMinima.x)
+        {
+            transform.position = posicionInicial;
+            velocidad++;
+        }
+    }
+}
